c2b659cb80d4b9dfc074e3fef10ca509a78cf40b
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-08-22 20:15:09 -0300

    chore: Rename main file

--- src/br/udesc/dsd/rmts/RoadMesh.java
+++ src/br/udesc/dsd/rmts/RoadMesh.java
@@ -1,8 +1,9 @@
-package br.udesc.dsd.rmts.view;
+package br.udesc.dsd.rmts;
 
 import br.udesc.dsd.rmts.controller.MeshController;
+import br.udesc.dsd.rmts.view.MainFrame;
 
-public class Main {
+public class RoadMesh {
     public static void main(String[] args) {
         MeshController meshController = new MeshController();
 
